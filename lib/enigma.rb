ee1a95397799df3628b4cd05799fc43ae86dd018
Kris Litman <duguy8@gmail.com>
2021-01-15 13:49:00 -0500

    Implemented stub in generate key test, need to build out functionality in enigma lib file

---
+++ lib/enigma.rb
@@ -0,0 +1,6 @@
+require 'date'
+
+class Enigma
+
+
+end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -9,14 +9,36 @@ class EnigmaTest < Minitest::Test
     assert_instance_of Enigma, enigma
   end
 
-  def test_enigma_can_encrypt_message
+  def test_enigma_can_generate_key
     skip
     enigma = Enigma.new
     enigma.stubs(:encrypt).returns({
                                     encryption: "Foo Bar",
-                                    key: "11111",
+                                    key: "01592",
                                     date: "011521"
                                   })
+    assert_equal "01592", enigma.generate_key
+  end
 
+  def test_enigma_can_find_todays_date
+    skip
+    enigma = Enigma.new
+    enigma.stubs(:encrypt).returns({
+                                    encryption: "Foo Bar",
+                                    key: "01592",
+                                    date: "011521"
+                                  })
+    assert_equal "01592", enigma.generate_key
+  end
+
+  def test_enigma_can_encrypt_message
+    skip
+    enigma = Enigma.new
+    enigma.encrypt({
+                    encryption: "Foo Bar",
+                    key: "11111",
+                    date: "011521"
+                  })
+    assert_equal "Foo Bar"
   end
 end
