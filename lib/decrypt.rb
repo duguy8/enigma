f60879616894a6126e8d23eefa4ebb5c2bd1810c
Kris Litman <duguy8@gmail.com>
2021-01-16 20:46:48 -0500

    Created runner files, need to practice ARGV and implement arguments

---
+++ lib/decrypt.rb
@@ -0,0 +1,5 @@
+#needs to take 4 command line arguments
+#1 existing file with an encrypted message
+#2 file where your program should write decrypted message
+#3 key to be used for decryption
+#4 date used for decryption
---
+++ lib/encrypt.rb
@@ -0,0 +1,5 @@
+#needs two command line arguments
+#1 existing file that contains a message to encrypt
+#2 file where the program should write encrypted message to
+
+#program should output to the screen file it wrote to, key & date
