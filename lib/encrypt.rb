ba78d1f534fc87cc7c0ce2573e477cca90cdca75
Kris Litman <duguy8@gmail.com>
2021-01-16 21:54:12 -0500

    Created message text file, also starting to build out the encrypt ruby file to be able to encrypt write a new encrypted file

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -3,3 +3,13 @@
 #2 file where the program should write encrypted message to
 
 #program should output to the screen file it wrote to, key & date
+require_relative 'enigma'
+require_relative 'encryption'
+
+file = File.open(ARGV[0], 'r')
+
+incoming = file.read
+
+puts incoming
+
+file.close
